3538a18abb3405432f28cbcd1851152b59cfb528
valtryn <valtryn@protonmail.com>
2024-09-26 17:16:06 +0800

    feat: add a few utils function
    Added the following functions:
    
    - utils_posix_base
    - utils_posix_file_type
    - utils_posix_path_abs
    - utils_posix_list_dir

--- utils.c
+++ utils.c
@@ -1,8 +1,17 @@
-#include "str.h"
-#include <stdio.h>
-#include <stdlib.h>
+// NOTE: not really sure what the side effects are if I define this.
+#define _XOPEN_SOURCE 500
 
-String* path_posix_base(const String *path)
+#include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "str.h"
+#include "utils.h"
+
+
+String* utils_posix_base(const String *path)
 {
 	StringArray *sa = NULL;
 	String sep;
@@ -35,4 +44,72 @@ exit1:
 	str_clear(&sep);
 exit2:
 	return base;
+}
+
+enum FileType utils_posix_file_type(String *abspath)
+{
+	enum FileType ft = FILE_UNKNOWN;
+	str_normalize(abspath);
+
+	struct stat s;
+	if (stat((const char *)str_view(abspath), &s) != 0) {
+		// TODO: print error log
+		goto exit;
+	}
+
+	if (S_ISREG(s.st_mode)) {
+		ft = FILE_REGULAR;
+		goto exit;
+	} else if(S_ISDIR(s.st_mode)) {
+		ft = FILE_DIRECTORY;
+		goto exit;
+	} else if(S_ISLNK(s.st_mode)) {
+		// TODO: test this
+		ft = FILE_SYMLINK;
+		goto exit;
+	}
+exit:
+	return ft;
+}
+
+ String* utils_posix_path_abs(String *path)
+{
+	String *abs = str_init();
+	str_normalize(path);
+	char *rp = realpath((const char *)str_view(path), NULL);
+	if (rp == NULL) {
+		str_free(abs);
+		return NULL;
+	}
+	str_clone_from_cstr(abs, rp);
+	free(rp);
+	return abs;
+}
+
+
+StringArray* _utils_posix_list_dir_cap(String *path, int capacity)
+{
+	DIR *dir = NULL;
+	struct dirent *entries;
+	StringArray *sa = NULL;
+	str_normalize(path);
+	dir = opendir(str_view_cstr(path));
+	if (dir == NULL) {
+		return NULL;
+	}
+	
+	sa = malloc(sizeof(StringArray));
+	str_array_init(sa, capacity);
+	while ((entries = readdir(dir)) != NULL) {
+		String temp;
+		str_clone_from_cstr(&temp, entries->d_name);
+		str_array_append_move(sa, &temp);
+	}
+	closedir(dir);
+	return sa;
+}
+
+StringArray* utils_posix_list_dir(String *path)
+{
+	return _utils_posix_list_dir_cap(path, 1<<8);
 }
--- utils.h
+++ utils.h
@@ -3,6 +3,21 @@
 
 #include "str.h"
 
-String* path_posix_base(const String *str);
+enum FileType {
+	FILE_UNKNOWN = 0,
+	FILE_REGULAR,
+	FILE_DIRECTORY,
+	FILE_SYMLINK,
+};
+
+String* utils_posix_base(const String *str);
+enum FileType utils_posix_file_type(String *abspath);
+String* utils_posix_path_abs(String *path);
+StringArray* utils_posix_list_dir(String *path);
+
+String* utils_win_base(const String *str);
+enum FileType utils_win_file_type(String *abspath);
+String* utils_posix_win_abs(String *path);
+StringArray* utils_win_list_dir(String *path);
 
 #endif /* UTILS_H */
